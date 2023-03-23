6637d6c236ddf5f6805167f23277e8961c03da95
manish-dev-1743 <devmanish1743@gmail.com>
2023-03-23 20:20:55 +0545

    Migration table added

---
+++ database/migrations/2023_03_23_142138_create_employee_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id('employee_id');
+            $table->string('employee_name',50);
+            $table->date('join_date')->nullable();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('role_id')->on('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+}
---
+++ database/migrations/2023_03_23_142815_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id('sales_id');
+            $table->bigInteger('sold_to_user_id');
+            $table->string('sold_to_user_name',100);
+            $table->decimal('sold_quantity');
+            $table->decimal('sold_amount');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('product_id')->on('product');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
